0bb6ad7e13f8600d076e1fa0f8004262d286c9bb
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2023-05-23 15:35:29 +0200

    Update download.py

--- download.py
+++ download.py
@@ -6,22 +6,30 @@ import zipfile
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type=str, default="./results")
+    parser.add_argument("--dataset", type=str, default="image")
     args = parser.parse_args()
     return args
 
 
 def main(args):
     print('')
-    urls = ['https://figshare.com/ndownloader/files/40694132']
-    pathlib.Path(f'{args.path}').mkdir(parents=True, exist_ok=True)
-    for url in urls:
-        r = requests.get(url, stream=True)
-        with open(f"{args.path}/data_transf_repr.zip", "wb") as f:
-            f.write(r.content)
+    
+    if args.dataset == 'image':
+    
+        urls = ['https://figshare.com/ndownloader/files/40694132']
+        pathlib.Path(f'{args.path}').mkdir(parents=True, exist_ok=True)
+        for url in urls:
+            r = requests.get(url, stream=True)
+            with open(f"{args.path}/data_transf_repr.zip", "wb") as f:
+                f.write(r.content)
 
-    with zipfile.ZipFile(f"{args.path}/data_transf_repr.zip", 'r') as zip_ref:
-        zip_ref.extractall(f"{args.path}")
+        with zipfile.ZipFile(f"{args.path}/data_transf_repr.zip", 'r') as zip_ref:
+            zip_ref.extractall(f"{args.path}")
 
+    elif args.dataset == 'protein':
+        pass
+    
+    
 if __name__ == "__main__":
     args = parse_arguments()
     main(args)
