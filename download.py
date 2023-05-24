b85c520414a879d448d2feea3407e2552c422bb8
valerianilucrezia <lucreziavaleriani@MacBook-Air-di-Lucrezia.local>
2023-05-24 13:31:58 +0200

    modify download.py to protein

--- download.py
+++ download.py
@@ -11,23 +11,33 @@ def parse_arguments():
     return args
 
 
-def main(args):
-    print('')
-    
+def main(args):    
     if args.dataset == 'image':
+        print('Downloading image dataset')
     
         urls = ['https://figshare.com/ndownloader/files/40694132']
         pathlib.Path(f'{args.path}').mkdir(parents=True, exist_ok=True)
         for url in urls:
             r = requests.get(url, stream=True)
-            with open(f"{args.path}/data_transf_repr.zip", "wb") as f:
+            with open(f"{args.path}/data_transf_repr_image.zip", "wb") as f:
                 f.write(r.content)
 
-        with zipfile.ZipFile(f"{args.path}/data_transf_repr.zip", 'r') as zip_ref:
+        with zipfile.ZipFile(f"{args.path}/data_transf_repr_image.zip", 'r') as zip_ref:
             zip_ref.extractall(f"{args.path}")
 
     elif args.dataset == 'protein':
-        pass
+        print('Downloading protein dataset')
+
+        urls = ['https://figshare.com/ndownloader/articles/23137958/versions/1']
+        pathlib.Path(f'{args.path}').mkdir(parents=True, exist_ok=True)
+        for url in urls:
+            r = requests.get(url, stream=True)
+            with open(f"{args.path}/data_transf_repr_protein.zip", "wb") as f:
+                f.write(r.content)
+
+        with zipfile.ZipFile(f"{args.path}/data_transf_repr_protein.zip", 'r') as zip_ref:
+            pathlib.Path(f"{args.path+'/data'}").mkdir(parents=True, exist_ok=True)
+            zip_ref.extractall(f"{args.path+'/data'}")
     
     
 if __name__ == "__main__":
